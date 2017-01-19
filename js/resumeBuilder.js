60665af11c05eee825d9adbf6378e9a67451d03d
Nate <nhargitt@bayareanewsgroup.com>
2017-01-19 13:20:18 -0800

    beautify my code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,38 +1,39 @@
 var bio = {
-    "welcomeMessage" :"Welcome to Jurassic Park",
-    "name" : "Nate Hargitt",
-    "role" : "Front End Web Develoer",
-    "contacts" : {
-        "mobile" : "666-666-6666",
-        "email" : "fakeAssGangsta@gmail.com",
-        "github" : "github.com/leefeetr",
-        "twitter" : "@doILook12",
-        "location" : "San Jose, 95126",
+    "welcomeMessage": "Welcome to Jurassic Park",
+    "name": "Nate Hargitt",
+    "role": "Front End Web Develoer",
+    "contacts": {
+        "mobile": "666-666-6666",
+        "email": "fakeAssGangsta@gmail.com",
+        "github": "github.com/leefeetr",
+        "twitter": "@doILook12",
+        "location": "San Jose, 95126",
         "blog": "NateHargitt.com/Blog"
     },
-    "Welcome Message" : "hello and welcom to my resume",
-    "skills" : ['web development', 'guitar', 'git', 'protecting princesses', 'coming back from the dead'],
-    "biopic" : 'images/me.png',
-    display : function() {
+    "Welcome Message": "hello and welcom to my resume",
+    "skills": ['web development', 'guitar', 'git', 'protecting princesses', 'coming back from the dead'],
+    "biopic": 'images/me.png',
+    display: function() {
         insertTemplateData("#header", HTMLheaderName, bio.name);
-         insertTemplateData("#header", HTMLheaderRole, bio.role);
-         insertTemplateData("#header", HTMLbioPic, bio.biopic);
-         insertTemplateData("#header", HTMLwelcomeMsg, bio.welcomeMessage);
+        insertTemplateData("#header", HTMLheaderRole, bio.role);
+        insertTemplateData("#header", HTMLbioPic, bio.biopic);
+        insertTemplateData("#header", HTMLwelcomeMsg, bio.welcomeMessage);
 
-    if (bio.skills) {
-    $('#header').append(HTMLskillsStart);
-        for(i = 0; i < bio.skills.length; i++) {
-            insertTemplateData("#skills", HTMLskills, bio.skills[i]);
+        if (bio.skills) {
+            $('#header').append(HTMLskillsStart);
+            for (var i = 0; i < bio.skills.length; i++) {
+                insertTemplateData("#skills", HTMLskills, bio.skills[i]);
+            }
         }
-    }
-    function addContacts (pageID) {
-    var contactInfo = bio.contacts;
-        insertTemplateData(pageID, HTMLmobile, contactInfo.mobile);
-        insertTemplateData(pageID, HTMLemail, contactInfo.email);
-        insertTemplateData(pageID, HTMLtwitter, contactInfo.twitter);
-        insertTemplateData(pageID, HTMLgithub, contactInfo.github);
-        insertTemplateData(pageID, HTMLblog, contactInfo.blog);
-        insertTemplateData(pageID, HTMLlocation, contactInfo.location);
+
+        function addContacts(pageID) {
+            var contactInfo = bio.contacts;
+            insertTemplateData(pageID, HTMLmobile, contactInfo.mobile);
+            insertTemplateData(pageID, HTMLemail, contactInfo.email);
+            insertTemplateData(pageID, HTMLtwitter, contactInfo.twitter);
+            insertTemplateData(pageID, HTMLgithub, contactInfo.github);
+            insertTemplateData(pageID, HTMLblog, contactInfo.blog);
+            insertTemplateData(pageID, HTMLlocation, contactInfo.location);
         }
         addContacts('#topContacts');
         addContacts('#footerContacts');
@@ -40,138 +41,122 @@ var bio = {
 
 };
 var work = {
-    jobs : [
-        {
-            "employer" : "ASHO",
-            "title" : "Sales and Delivery Manager",
-            "location" : "Los Angeles",
-            "zip": "90027",
-            "dates" : "2006 - 2013",
-            "description" : "Incharge of making all the cash money money"
-        },
-        {
-            "employer" : "PAC Consturction",
-            "title" : "Finishes Manager",
-            "location" : "Los Angeles",
-            "zip" : "90210",
-            "dates" : "2013 - 2014",
-            "description" : "Make that drywall look like it should"
-        },
-        {
-            "employer" : "Exxell Fire Systems",
-            "title" : "Sales and Service",
-            "location" : "Oakland",
-            "zip" : "94610",
-            "dates" : "2014 - 2015",
-            "description" : "Sold Fire Protection and installed"
-        },
-        {
-            "employer" : "Appliance Repair Doctor",
-            "title" : "Front End Web Develoer",
-            "location" : "Alameda",
-            "zip" : "94501",
-            "dates" : "Feb 2016 - June 2016",
-            "description" : "Built the website and managed it"
-        },
-        {
-            "employer" : "High Five",
-            "title" : "Front End Web Develoer",
-            "location" : "Redwood City",
-            "zip" : "94061",
-            "dates" : "June 2016 - Oct 2016",
-            "description" : "Built the website and managed it"
-        },
-        {
-            "employer" : "Bay Area News Group",
-            "title" : "Front End Web Develoer",
-            "location" : "San Jose",
-            "zip" : "95126",
-            "dates" : "November 2016 - Present",
-            "description" : "Built the website and managed it"
-        },
-    ],
+    jobs: [{
+        "employer": "ASHO",
+        "title": "Sales and Delivery Manager",
+        "location": "Los Angeles",
+        "zip": "90027",
+        "dates": "2006 - 2013",
+        "description": "Incharge of making all the cash money money"
+    }, {
+        "employer": "PAC Consturction",
+        "title": "Finishes Manager",
+        "location": "Los Angeles",
+        "zip": "90210",
+        "dates": "2013 - 2014",
+        "description": "Make that drywall look like it should"
+    }, {
+        "employer": "Exxell Fire Systems",
+        "title": "Sales and Service",
+        "location": "Oakland",
+        "zip": "94610",
+        "dates": "2014 - 2015",
+        "description": "Sold Fire Protection and installed"
+    }, {
+        "employer": "Appliance Repair Doctor",
+        "title": "Front End Web Develoer",
+        "location": "Alameda",
+        "zip": "94501",
+        "dates": "Feb 2016 - June 2016",
+        "description": "Built the website and managed it"
+    }, {
+        "employer": "High Five",
+        "title": "Front End Web Develoer",
+        "location": "Redwood City",
+        "zip": "94061",
+        "dates": "June 2016 - Oct 2016",
+        "description": "Built the website and managed it"
+    }, {
+        "employer": "Bay Area News Group",
+        "title": "Front End Web Develoer",
+        "location": "San Jose",
+        "zip": "95126",
+        "dates": "November 2016 - Present",
+        "description": "Built the website and managed it"
+    }, ],
     display: function() {
-        for(var job = 0; job < work.jobs.length; job++) {
-        $('#workExperience').append(HTMLworkStart);
-        var workInfo = replaceTemplateInfo( HTMLworkEmployer, work.jobs[job].employer ) +
-        replaceTemplateInfo( HTMLworkTitle, work.jobs[job].title ) +
-        replaceTemplateInfo( HTMLworkDates, work.jobs[job].dates ) +
-        replaceTemplateInfo( HTMLworkLocation, work.jobs[job].location ) +
-        replaceTemplateInfo( HTMLworkDescription, work.jobs[job].description )
-        $('.work-entry:last').append(workInfo);
-    }
+        for (var job = 0; job < work.jobs.length; job++) {
+            $('#workExperience').append(HTMLworkStart);
+            var workInfo = replaceTemplateInfo(HTMLworkEmployer, work.jobs[job].employer) +
+                replaceTemplateInfo(HTMLworkTitle, work.jobs[job].title) +
+                replaceTemplateInfo(HTMLworkDates, work.jobs[job].dates) +
+                replaceTemplateInfo(HTMLworkLocation, work.jobs[job].location) +
+                replaceTemplateInfo(HTMLworkDescription, work.jobs[job].description);
+            $('.work-entry:last').append(workInfo);
+        }
     }
 };
 var projects = {
-    projects : [
-        {
-            "name" : "Javscript BlackJack",
-            "dates" : "Dec 2016",
-            "description" : "A JavaScript renditoin of the great game BlackJack",
-            "images" : ['http://natehargitt.com/assets/images/blackjack.png']
-        },
-        {
-            "name" : "Pro Modeling Portfolio",
-            "dates" : "Dec 2016",
-            "description" : "A prorfolio for the top model",
-            "images" : ["http://natehargitt.com/assets/images/kaiarose.png"]
-        }
-    ],
-    display : function() {
+    projects: [{
+        "name": "Javscript BlackJack",
+        "dates": "Dec 2016",
+        "description": "A JavaScript renditoin of the great game BlackJack",
+        "images": ['http://natehargitt.com/assets/images/blackjack.png']
+    }, {
+        "name": "Pro Modeling Portfolio",
+        "dates": "Dec 2016",
+        "description": "A prorfolio for the top model",
+        "images": ["http://natehargitt.com/assets/images/kaiarose.png"]
+    }],
+    display: function() {
         var projectsArr = projects.projects;
 
-        for(i = 0; i < projectsArr.length; i++) {
+        for (var i = 0; i < projectsArr.length; i++) {
 
             $('#projects').append(HTMLprojectStart);
             var projectClass = $(".project-entry:last");
             insertTemplateData(projectClass, HTMLprojectTitle, projectsArr[i].name);
             insertTemplateData(projectClass, HTMLprojectDates, projectsArr[i].dates);
             insertTemplateData(projectClass, HTMLprojectDescription, projectsArr[i].description);
-            for(i = 0; i < projectsArr[i].images.length; i++) {
-            insertTemplateData(projectClass, HTMLprojectImage, projectsArr[i].images);
+            for (var i = 0; i < projectsArr[i].images.length; i++) {
+                insertTemplateData(projectClass, HTMLprojectImage, projectsArr[i].images);
             }
         }
     }
 };
 var education = {
-    schools : [
-        {
-            "name" : "Codify Academy",
-            "location" : "San Francisco",
-            "degree" : "Front End Development",
-            "dates" : "Sept 2015 - Jan 2016",
-            "majors" : ["Front End Web Development"],
-            "url" : "http://CodifyAcademy.com"
-        },
-        {
-            "name" : "Armstrong",
-            "location" : "Plymoth",
-            "degree" : "Wasted Time",
-            "dates" : "Sep 2004 - Oct 2006",
-            "majors" : ["Socializing", "Dating", "Music"],
-            "url" : "http://armstrongHigh.com"
-        },
-    ],
-    onlineCourses :  [
-            {
-                "title" : "JavaScript Basics",
-                "name" : "Code Academy",
-                "dates" : "Dec 2016",
-                "url" : "http:codeacademy.com"
-            },
-            {
-                "title" : "NodeJS Basics",
-                "name" : "Code School",
-                "dates" : "Dec 2016",
-                "url" : "CodeSchool.com"
-            }
-        ],
-    displaySchools : function() {
-            var schoolsArr = education.schools;
-            var onlineClassArr = education.onlineCourses;
+    schools: [{
+        "name": "Codify Academy",
+        "location": "San Francisco",
+        "degree": "Front End Development",
+        "dates": "Sept 2015 - Jan 2016",
+        "majors": ["Front End Web Development"],
+        "url": "http://CodifyAcademy.com"
+    }, {
+        "name": "Armstrong",
+        "location": "Plymoth",
+        "degree": "Wasted Time",
+        "dates": "Sep 2004 - Oct 2006",
+        "majors": ["Socializing", "Dating", "Music"],
+        "url": "http://armstrongHigh.com"
+    }, ],
+    onlineCourses: [{
+        "title": "JavaScript Basics",
+        "name": "Code Academy",
+        "dates": "Dec 2016",
+        "url": "http:codeacademy.com"
+    }, {
+        "title": "NodeJS Basics",
+        "name": "Code School",
+        "dates": "Dec 2016",
+        "url": "CodeSchool.com"
+    }],
+    displaySchools: function() {
+        var schoolsArr = education.schools;
+        var onlineClassArr = education.onlineCourses;
 
 
-        for(i = 0; i < schoolsArr.length; i++) {
+        for (i = 0; i < schoolsArr.length; i++) {
 
             $('#education').append(HTMLschoolStart);
             var educationClass = $(".education-entry:last");
@@ -182,15 +167,15 @@ var education = {
             insertTemplateData(educationClass, HTMLschoolMajor, schoolsArr[i].majors);
         }
         $('#education').append(HTMLonlineClasses);
-        for(i = 0; i < onlineClassArr.length; i++) {
+        for (i = 0; i < onlineClassArr.length; i++) {
 
 
             $('#education').append(HTMLschoolStart);
-            var educationClass = $(".education-entry:last");
-            insertTemplateData(educationClass, HTMLschoolName, onlineClassArr[i].title);
-            insertTemplateData(educationClass, HTMLonlineTitle, onlineClassArr[i].name);
-            insertTemplateData(educationClass, HTMLonlineSchool, onlineClassArr[i].dates);
-            insertTemplateData(educationClass, HTMLonlineURL, onlineClassArr[i].url);
+            var lastEducationClass = $(".education-entry:last");
+            insertTemplateData(lastEducationClass, HTMLschoolName, onlineClassArr[i].title);
+            insertTemplateData(lastEducationClass, HTMLonlineTitle, onlineClassArr[i].name);
+            insertTemplateData(lastEducationClass, HTMLonlineSchool, onlineClassArr[i].dates);
+            insertTemplateData(lastEducationClass, HTMLonlineURL, onlineClassArr[i].url);
         }
 
     }
@@ -214,12 +199,13 @@ education.displaySchools();
 
 
 //Helper functions
-function replaceTemplateInfo (templateVar, newValue) {
-    return templateVar.replace("%data%", newValue)
+function replaceTemplateInfo(templateVar, newValue) {
+    return templateVar.replace("%data%", newValue);
 }
+
 function insertTemplateData(pageId, templateVar, newValue) {
     var info = replaceTemplateInfo(templateVar, newValue);
-    $(pageId).append(info)
+    $(pageId).append(info);
 }
 
 
@@ -230,6 +216,3 @@ function displayMap() {
     $('#mapDiv').append(googleMap);
 }
 displayMap();
-
-
-
